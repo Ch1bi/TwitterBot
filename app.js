af5e48200f7a422e07594415bbc9f7ac6940c875
ch1bi <maine1191@gmail.com>
2016-12-21 10:41:52 -0500

    added favorited tweets

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ var config = require("./config.js");
 
 var Twitter = new twit(config);
 
-var reTweet = function()
+var retweet = function()
 {
     //create an object of parameters for the search tweet function
     var parameters = 
         {
-            q: "#fashion", "#fashion", "#vogue", "#blog",
+            q: "#fashion, #fashion, #vogue, #blog",
             result_type: "recent",
             lang:"en"
         }
@@ -25,13 +25,13 @@ var reTweet = function()
         if(!error)
         {
             //get the id of the tweet
-            var reTweetId = data.statuses[0].id_str;
+            var retweetId = data.statuses[0].id_str;
             
             //then tell Twitter to retweet 
             Twitter.post("statuses/retweet/:id",
-                         {
+            {
                 id: retweetId
-            }, function(error response)
+            }, function(error, response)
                          {
                 if(response)
                 {
@@ -48,8 +48,62 @@ var reTweet = function()
                          else
                          {
                           console.log("Something went wrong while searching");
-                         }
-        
-        
+                         }   
     });
 }
+////////////////////////////////////////////////////////////////////////
+//find a random tweet to favorite
+var favoriteTweet = function()
+{
+    var parameters = 
+        {
+            q: "#fashion, #fashion, #vogue, #blog",
+            result_type: "recent",
+            lang:"en"
+        }
+    
+    //find the tweet
+    Twitter.get("search/tweets", parameters, function(error, data)
+                {
+        //find the tweets
+        var tweet = data.statuses;
+        var randomTweet = randTweet(tweet);
+        
+        //if random tweet exists
+        if(!typeof randomTweet != "undefined")
+        {
+            Twitter.post("favorites/create", {id:randomTweet.id_str}, function(error, response)
+                         {
+                //if error while response
+                if(error)
+                {
+                    console.log("can't be favorited")
+                }
+                
+                else
+                {
+                    console.log("favorited");
+                }
+            });
+        }
+    });
+}
+
+//grab favorite tweet when program starts
+favoriteTweet();
+
+//set a favorite every 10 mins;
+setInterval(favoriteTweet, 600000);
+
+//function to grab random tweet
+function randTweet(arr)
+{
+    var index = Math.floor(Math.random() * arr.length);
+    return arr[index];
+}
+
+//retweet as soon as program starts
+retweet();
+setInterval(retweet, 300000);
+
+//follow partmb
